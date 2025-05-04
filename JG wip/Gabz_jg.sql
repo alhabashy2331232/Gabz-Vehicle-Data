a8d131e03ea52e4ff22c96ac78dc519d82571f36
JustBlueDolphin <61672613+JustBlueDolphin@users.noreply.github.com>
2025-05-04 14:46:07 +0100

    Update Gabz_jg.sql

--- JG wip/Gabz_jg.sql
+++ JG wip/Gabz_jg.sql
@@ -1,5 +1,4 @@
 INSERT INTO `dealership_vehicles` (`spawn_code`, `brand`, `model`, `hashkey`, `category`, `price`, `created_at`) VALUES
-	('gb200', 'Vapid', 'GB 200', '1909189272', 'sports', 74534, '2024-12-25 19:57:40'),
 	('gb811s2', 'PFISTER', 'gb811s2', 'gb811s2', 'sports', 0, '2024-12-25 19:57:42'),
 	('gbadmiral', 'nill', 'gbadmiral', 'gbadmiral', 'nill', 0, '2025-04-04 22:09:26'),
 	('gbargento7f', 'OBEY', 'gbargento7f', 'gbargento7f', 'sports', 0, '2024-12-25 19:57:42'),
